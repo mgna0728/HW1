184e939068b98a2c44e36f491b8d8fb76c92143b
mgna0728 <mgna0728@gmail.com>
2022-10-06 03:18:58 +0800

    2.25

---
+++ 2.25.cs
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    printf("pppppppppp\n");
+    printf("     p   p\n");
+    printf("     p   p\n");
+    printf("     p   p\n");
+    printf("      p p \n");
+    printf("          \n");
+    printf("  ]]      \n");
+    printf(" ]        \n");
+    printf("]         \n");
+    printf(" ]        \n");
+    printf("  ]]]]]]]]\n");
+    printf("          \n");
+    printf("DDDDDDDDDD\n");
+    printf("D        D\n");
+    printf("D        D\n");
+    printf(" D      D\n");
+    printf("  DDDDDD\n");
+    return 0;
+}
