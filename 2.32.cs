d36192eefe9e90b75f5b816a2b4fe739498d9e16
mgna0728 <mgna0728@gmail.com>
2022-10-06 03:16:18 +0800

    three files

---
+++ 2.32.cs
@@ -0,0 +1,10 @@
+#include<stdlib.h>
+#include<stdio.h>
+int main()
+{
+    int i;
+    for (i=0;i<=10;i++){
+        printf("%d\t %d\t %d\n",i,i*i,i*i*i);
+    }
+    return 0;
+}
---
+++ 2.33.cs
@@ -0,0 +1,9 @@
+#include<stdlib.h>
+#include<stdio.h>
+int main()
+{
+    float a,b,c,d,e;
+    scanf("%f%f%f%f%f",&a,&b,&c,&d,&e);
+    printf("%f",d+e+(a*b/c));
+    return 0;
+}
